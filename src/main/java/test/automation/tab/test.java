d676f4f05704cb4b48b12365a397f161488abe0d
zakir08-tech <54797321+zakir08-tech@users.noreply.github.com>
2023-06-03 12:04:43 +0530

    [060320231204]: first commit to maste
    [060320231204]: first commit to master

---
+++ src/main/java/test/automation/tab/test.java
@@ -0,0 +1,43 @@
+package test.automation.tab;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import com.automation.bolt.common;
+import com.automation.bolt.glueCode;
+
+public class test {
+	public static WebDriver xDriver;
+	
+	@SuppressWarnings("static-access")
+	public static void main(String[] args) throws IOException {
+		
+		glueCode gCode = new glueCode();
+		common con = new common();
+		glueCode.implicitWaitTime =120;
+		glueCode.pageLoadTimeOut =120;
+		glueCode.getWebDriver("chrome");
+		
+		xDriver =gCode.boltDriver;
+		String xDays ="5";
+		
+		/* add a break-point and open the required URL in debug mode */
+		xDriver.get("https://tab123.com/");
+		/* then navigate to the required page manually for which userDefine code needs to be executed*/
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+		Calendar c = Calendar.getInstance();
+		c.setTime(new Date());
+		c.add(Calendar.DATE, Integer.valueOf(xDays));
+		String newDate = sdf.format(c.getTime());
+		
+		WebElement setNewDate =xDriver.findElement(By.xpath("//input[@id='endDate']"));
+		con.keySet(setNewDate, newDate);
+	}
+}
---
+++ src/main/java/test/automation/tab/userDefine.java
@@ -0,0 +1,44 @@
+package test.automation.tab;
+
+import com.automation.bolt.common;
+import com.automation.bolt.glueCode;
+import com.automation.bolt.htmlReportCommon;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+	
+public class userDefine extends glueCode{
+	public static WebDriver xDriver;
+	
+	@SuppressWarnings("static-access")
+	public static void calcualteNewDateAndSet(String[] args) {
+		/* predefine classes can be used to get the current test run-driver and other required methods */
+		glueCode gCode = new glueCode();
+		common con = new common();
+		htmlReportCommon htmlReport = new htmlReportCommon();
+		
+		xDriver=gCode.boltDriver;
+		String xDays =args[0];
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+		Calendar c = Calendar.getInstance();
+		c.setTime(new Date());
+		c.add(Calendar.DATE, Integer.valueOf(xDays));
+		String newDate = sdf.format(c.getTime());
+		
+		WebElement setNewDate =xDriver.findElement(By.xpath("//input[@id='endDate']"));
+		con.keySet(setNewDate, newDate);
+		
+		/* if required predefine methods can be used to add step description into the word document and html report */
+		con.writeStepInWordDocument("set new date: " +newDate);
+		htmlReport.writeStepInHtmlReport("set new date: " +newDate);
+		
+		/* if required predefine methods can be used to add sceen-shot into the word document and html report */
+		con.attachScreenShotInWordDocument();
+		htmlReport.attachScreenShotInHtmlReport();
+	}	
+}
